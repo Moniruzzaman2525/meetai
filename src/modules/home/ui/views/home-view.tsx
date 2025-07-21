b47fc59e81b75ac91fe49208093ccea370c71919
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-07-21 13:24:40 +0600

    09 agents setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,17 +1,11 @@
 'use client';
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
 
 
 export default function HomeView() {
-    const trpc = useTRPC();
-    const { data } = useQuery(trpc.hello.queryOptions({ text: "Monir" }))
     return (
-        <div className="flex flex-col p-4 gap-y-4">
-            {
-                data?.greeting
-            }
+        <div>
+            Home view
         </div>
     );
 }
