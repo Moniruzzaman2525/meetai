66f4293805d08a4b74c61f3c176bfcd9a5506134
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-07-23 00:33:54 +0600

    14 agent id view

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -5,15 +5,21 @@ import { agentsInsertSchema } from "../schemas";
 import { z } from "zod";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
+import { TRPCError } from "@trpc/server";
 
 export const agentRouter = createTRPCRouter({
 
-    getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
+    getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input, ctx }) => {
         const [existingAgent] = await db.select({
             ...getTableColumns(agents),
             // meetingCount: db.$count(agents, eq(agents.userId, ctx.auth.session.userId))
             meetingCount: sql<number>`5`
-        }).from(agents).where(eq(agents.id, input.id));
+        }).from(agents).where(and(eq(agents.id, input.id), eq(agents.userId, ctx.auth.user.id)));
+
+        if (!existingAgent) {
+            throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" })
+        }
+
         return existingAgent
     }),
     getMany: protectedProcedure.input(z.object({
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -8,8 +8,10 @@ import { columns } from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { DataPagination } from "../components/data-pagination";
+import { useRouter } from "next/navigation";
 
 export const AgentsViews = () => {
+    const router = useRouter()
     const [filters, setFilters] = useAgentsFilters();
     const trpc = useTRPC()
     const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
@@ -18,7 +20,7 @@ export const AgentsViews = () => {
 
     return (
         <div className="flex-1 pb-4 mx-4 md:px-8 flex flex-col gap-y-4">
-            <DataTable data={data.items} columns={columns} />
+            <DataTable onRowClick={(row) => router.push(`/agents/${row.id}`)} data={data.items} columns={columns} />
             <DataPagination page={filters.page} totalPages={data.totalPage} onChangePage={(page) => setFilters({ page })} />
             {
                 data.items.length === 0 && (<EmptyState title="Create your first agent" description="Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call" />)
