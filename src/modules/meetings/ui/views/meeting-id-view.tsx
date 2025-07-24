152bf3b0d090099a720fe0dbdb6df960f1693afe
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-07-24 14:48:08 +0600

    21-meeting-variants

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -10,6 +10,10 @@ import { toast } from "sonner";
 import { useConfirm } from "@/hooks/use-confirm";
 import { UpdateMeetingsDialogue } from "../components/update-meeting-dialogue";
 import { useState } from "react";
+import { UpcomingState } from "../components/upcoming-state";
+import { ActiveState } from "../components/active-state";
+import { CancelledState } from "../components/cancelled-state";
+import { ProcessingState } from "../components/processing-state";
 
 interface Props {
     meetingId: string
@@ -47,12 +51,24 @@ export const MeetingIdView = ({ meetingId }: Props) => {
         await removeMeeting.mutateAsync({ id: meetingId })
     }
 
+
+    const isActive = data.status === "active"
+    const isUpcoming = data.status === "upcoming"
+    const isCancelled = data.status === "canceled"
+    const isCompleted = data.status === "completed"
+    const isProcessing = data.status === "processing"
+
     return (
         <>
             <RemoveConfirmation />
             <UpdateMeetingsDialogue open={updateMeetingDialogueOpen} onOpenChange={setUpdateMeetingDialogueOpen} initialValues={data} />
             <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
                 <MeetingIdViewHeader meetingId={meetingId} meetingName={data.name} onEdit={() => setUpdateMeetingDialogueOpen(true)} onRemove={handleRemoveMeeting} />
+                {isCancelled && <CancelledState />}
+                {isProcessing && <ProcessingState />}
+                {isCompleted && <div>Complete</div>}
+                {isActive && (<ActiveState meetingId={meetingId} />)}
+                {isUpcoming && (<UpcomingState meetingId={meetingId} onCancelMeeting={() => { }} isCancelling={false} />)}
             </div>
         </>
     );
